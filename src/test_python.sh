1f89f95975a4d452d247b2b9ad3b9af8cc1d350f
DenDen047 <sh.mn.nat@gmail.com>
2020-03-13 14:32:31 +0900

    success to start running the all test of python

--- src/test_python.sh
+++ src/test_python.sh
@@ -5,4 +5,4 @@ mkdir -p /tmp; cd $_
 git clone https://github.com/milvus-io/milvus
 cd milvus/tests/milvus_python_test
 
-pytest test_connect.py --level=1 --ip=milvus_server
+pytest . --level=1 --ip=milvus_server
