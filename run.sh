4baab4157c0159b5ad916898023f914795aec16f
DenDen047 <sh.mn.nat@gmail.com>
2020-03-13 10:29:50 +0900

    succeed to run Milvus with monitor tools

--- run.sh
+++ run.sh
@@ -4,13 +4,24 @@ export MILVUS_HOME=$(PWD)
 echo ${MILVUS_HOME}
 
 # donlowad configuration files
-mkdir -p conf
-cd conf
+mkdir -p ${MILVUS_HOME}/conf
+cd ${MILVUS_HOME}/conf
 if [[ ! -f ./server_config.yaml ]]; then
     wget https://raw.githubusercontent.com/milvus-io/milvus/v0.7.0/core/conf/demo/server_config.yaml
 fi
 if [[ ! -f ./log_config.conf ]]; then
     wget https://raw.githubusercontent.com/milvus-io/milvus/v0.7.0/core/conf/demo/log_config.conf
+fi
+
+cd ${MILVUS_HOME}/docker
+if [[ ! -f ./alertmanager.yml ]]; then
+    wget https://raw.githubusercontent.com/milvus-io/milvus/master/docker/alertmanager.yml
+fi
+if [[ ! -f ./prometheus.yml ]]; then
+    wget https://raw.githubusercontent.com/milvus-io/milvus/master/docker/prometheus.yml
+fi
+if [[ ! -f ./server_down.yml ]]; then
+    wget https://raw.githubusercontent.com/milvus-io/milvus/master/docker/server_down.yml
 fi
 
 # Start docker-compose
